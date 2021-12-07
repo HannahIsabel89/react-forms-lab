ea6f67f8d185c7ae7428f162edc79a0612e5334d
Hannah <hannahwoodward@hotmail.com>
2021-12-07 11:37:07 +0800

    done

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,14 +4,30 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      message: "", // need to first set the key/value pair in state
+    };
   }
 
+  messageWritten = event => {
+    this.setState({
+      message: event.target.value, // update the message to the event target
+    });
+  };
+
   render() {
+    let rmnChars = this.props.maxChars - this.state.message.length;
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input
+          type="text"
+          name="message"
+          id="message"
+          onChange={this.messageWritten}
+          value={this.state.message}
+        />
+        {rmnChars}
       </div>
     );
   }
